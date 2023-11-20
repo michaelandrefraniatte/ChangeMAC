9483df34e845d728f5682d158f2cd20dd1d3c492
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-11-20 20:00:16 +0100

    Initial commit

---
+++ Src/ChangeMAC/ChangeMAC/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstMacAddress = System.Net.NetworkInformation.NetworkInterface
+            .GetAllNetworkInterfaces()
+            .Where(nic => nic.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up && nic.NetworkInterfaceType != System.Net.NetworkInformation.NetworkInterfaceType.Loopback)
+            .Select(nic => nic.GetPhysicalAddress().ToString())
+            .FirstOrDefault();
+            Console.WriteLine("Network MAC Address :");
+            Console.WriteLine(firstMacAddress);
+            string str = firstMacAddress.Substring(0, 2);
+            string[] stritems = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+            Random rand = new Random();
+            int index = rand.Next(stritems.Length);
+            str = str + "-" + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + "-" + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + "-" + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + "-" + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + "-" + stritems[index];
+            index = rand.Next(stritems.Length);
+            str = str + stritems[index];
+            index = rand.Next(stritems.Length);
+            Console.WriteLine("Network New MAC Address :");
+            Console.WriteLine(str);
+            ChangeMacAddress(str);
+            Console.WriteLine("done");
+        }
+        public static void ChangeMacAddress(string macadress)
+        {
+            var start = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                RedirectStandardOutput = true,
+                Arguments = $"Set-NetAdapter -InterfaceDescription 'Realtek*' -MacAddress '{macadress}'",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            var process = Process.Start(start);
+            process.WaitForExit();
+        }
+    }
+}
