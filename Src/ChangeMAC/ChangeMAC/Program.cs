a2cd0bc63b398222a74939ec110004ed74b0855a
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-09 22:24:02 +0100

    Add about

--- Src/ChangeMAC/ChangeMAC/Program.cs
+++ Src/ChangeMAC/ChangeMAC/Program.cs
@@ -3,13 +3,45 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
+using KeyboardInputsAPI;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace ChangeMAC
 {
     internal class Program
     {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        public static uint getForegroundProcessPid()
+        {
+            uint processID = 0;
+            IntPtr hWnd = GetForegroundWindow();
+            GetWindowThreadProcessId(hWnd, out processID);
+            return processID;
+        }
+        public static void OnKeyDown()
+        {
+            KeyboardInput ki = new KeyboardInput();
+            ki.Scan();
+            ki.BeginPolling();
+            while (true)
+            {
+                if (ki.KeyboardKeyF1 & getForegroundProcessPid() == Process.GetCurrentProcess().Id)
+                {
+                    const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                    const string caption = "About";
+                    MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                System.Threading.Thread.Sleep(60);
+            }
+        }
         static void Main(string[] args)
         {
+            Task.Run(() => { OnKeyDown(); });
             string firstMacAddress = GetMAC();
             Console.WriteLine("Network MAC Address:");
             Console.WriteLine(firstMacAddress);
